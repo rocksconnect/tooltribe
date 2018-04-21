20497a806e4a95375e99213d7cfd98316a379c09
amit.synergytop <amit.yadav@synergytop.com>
2018-04-21 17:42:36 +0530

    cdoe commit

--- server.js
+++ server.js
@@ -104,7 +104,8 @@ app.use(function(req, res, next) {
                 '/getDetailsOfTool',
                 '/getHomeScreenData',
                 '/homeScreenSearch',
-                '/getBrandCategory'
+                '/getBrandCategory',
+                '/getUserProfile'
             ];
   //req.url == '/getAllList' || req.url == '/allIdProof' || req.url == '/getCompany' || req.url == '/updateIdProofType' || req.url == '/getTrade' || req.url == '/login' || req.url == '/userLogin' || req.url == '/register' || req.url == '/account_email_validation' || req.url == '/forgetPassword' || req.url == '/forgetPasswordReset' || req.url == '/' || req.url == '/terms' || req.url == '/privacy' || req.url == '/aboutus' || req.url == '/support' || req.url == '/getCategory'
 
